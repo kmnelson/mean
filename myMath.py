e2c4a57b761ff28d4592b724bc7bec628d2fc4c2
Kevin Nelson <kevnels@umich.edu>
2019-08-21 11:50:42 -0700

    Removed dependency on PyContract

--- myMath.py
+++ myMath.py
@@ -12,27 +12,15 @@ def mean(num_list):
         msg = "The algebraic mean of an non-numerical list is undefined.\nPlease provide a list of numbers."
         raise TypeError(detail.__str__() + "\n" +  msg)
 
-@contract(x='int,>=0',returns='int,>=0')
 def perfect_sqrt(x):
     retval = sqrt(x)
     iretval = int(retval)
     return iretval if iretval == retval else retval
 
 
-@new_contract
-def ends_ok(x):
-    ends14569 = x%10 in (1,4,5,6,9)
-    ends00 = int(round((log(x,10)))) % 2 == 0
-    if ends14569 or ends00:
-        return True
-    raise ValueError("%s doesn't end in 1,4,5,6 or 9 or even number of zeros"%x)
-
-
-@contract(x='int,ends_ok,>=0',returns='int,>=0')
 def perfect_sqrt2(x):
     return int(sqrt(x))
 
-@contract(n='int,>=0')
 def fib(n):
     if n == 0 or n == 1:
         return 1
